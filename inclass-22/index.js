0bf0bad54f99893c2a08bd4a7263577fde8ad600
Boyang Bai <BBYmac@Boyangs-MacBook-Pro.local>
2016-11-08 19:40:31 -0600

    minor changes

--- inclass-22/index.js
+++ inclass-22/index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
-const app = express()
+const cookieParser = require('cookie-parser')
 
 const CORS = (req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Credentials: true')
+	res.header('Access-Control-Allow-Credentials', true)
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-	res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With, Accept')
+	res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With')
 	next()
 }
 
+const app = express()
+
+app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(CORS)
-require('./profile')(app)
+
+require('./src/profile')(app)
+require('./src/articles')(app)
+require('./src/auth')(app)
+require('./src/following')(app)
 
 // Get the port from the environment, i.e., Heroku sets it
 const port = process.env.PORT || 3000
